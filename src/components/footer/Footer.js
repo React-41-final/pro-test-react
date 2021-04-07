0b182affdfee54f5f40a51fcf2886757e50b83af
Marik713 <62308344+Marik713@users.noreply.github.com>
2021-04-07 23:56:42 +0300

    ready footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import routes from "../../routers/routers";
+import styles from "./Footer.module.css";
+import sprite from "../../sprites/sprite.svg";
 
 const Footer = () => {
-  return <h2>Footer</h2>;
+  return (
+    <div className={styles.footer}>
+      <div className={styles.info}>
+        <span className={styles.infoUnit}>&copy; 2020</span>
+        <span className={styles.infoUnit}>All Rights Reserved</span>
+        <span className={styles.infoUnit}>
+          Developed with{" "}
+          <span className={styles.heartWrapper}>
+            <svg className={styles.heart}>
+              <use href={sprite + "#paintedHeart"} />
+            </svg>
+          </span>
+        </span>
+      </div>
+      <p className={styles.author}>
+        by{" "}
+        <NavLink to={routes.usefullInfo} className={styles.navLink}>
+          GoIT Students
+        </NavLink>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
