fd4ccb5884e9435a38a159688148f86e754778a0
Ivan <ilchenkoivan92@gmail.com>
2021-04-08 22:05:22 +0300

    made header with logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import Logo from '../logo/Logo';
-import Navigation from '../navigation/Navigation';
-import UserInfo from '../userInfo/UserInfo';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Logo from "../logo/Logo";
+import Navigation from "../navigation/Navigation";
+import UserInfo from "../userInfo/UserInfo";
+import sprite from "../../sprites/sprite.svg";
+import styles from "./Header.module.css";
 
-const Header = () => {
-  return (
-    <div>
-      <h2>Header</h2>
-      <Logo />
-      <Navigation />
-      <UserInfo />
-    </div>
-  );
-};
+class Header extends Component {
+  state = {
+    isModalOn: false,
+  };
+  handleModal = () => this.setState({ isModalOn: !this.state.isModalOn });
 
-export default Header;
+  render() {
+    const { isModalOn } = this.state;
+    const { isAuthorized } = this.props;
+    return (
+      <>
+        <div className={styles.container}>
+          <Logo />
+          <div className={styles.nav}>
+            <Navigation props={isAuthorized} />
+          </div>
+          {isAuthorized ? <UserInfo /> : false}
+          {isModalOn ? (
+            <div className={styles.burger}>
+              <svg className={styles.burgerOn} onClick={this.handleModal}>
+                <use href={sprite + "#close"} />
+              </svg>
+            </div>
+          ) : (
+            <div className={styles.burger}>
+              <svg className={styles.burgerOn} onClick={this.handleModal}>
+                <use href={sprite + "#burger"} />
+              </svg>
+            </div>
+          )}
+        </div>
+        {isModalOn ? <Navigation isAuthorized={isAuthorized} /> : false}
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isAuthorized: state.auth.token,
+});
+
+export default connect(mapStateToProps)(Header);
