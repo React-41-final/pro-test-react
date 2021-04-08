611af059d89639e57c1f3faa267f45ad5a00ed3a
AlexKozzlov <alexandr.kozlov1120@gmail.com>
2021-04-08 11:26:11 +0300

    route  usefullInfo --> usefulInfo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
       </div>
       <p className={styles.author}>
         by{" "}
-        <NavLink to={routes.usefullInfo} className={styles.navLink}>
+        <NavLink to={routes.usefulInfo} className={styles.navLink}>
           GoIT Students
         </NavLink>
       </p>
