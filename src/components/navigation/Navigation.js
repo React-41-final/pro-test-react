525d5e4eb449c4cd58e266b4da74451bd72b6a2f
Marik713 <62308344+Marik713@users.noreply.github.com>
2021-04-10 22:16:05 +0300

    ready navigation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Navigation.module.css";
+import routes from "../../routers/routers";
 
-const Navigation = () => {
-  return <h3>Navigation</h3>;
-};
+const Navigation = ({ isAuthorized = true }) => (
+  <nav className={styles.mainNav}>
+    <ul >
+      <li>
+        <NavLink
+          to={routes.mainPage}
+          exact
+          className={styles.link}
+          activeClassName={styles.activeLink}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={routes.test}
+          exact
+          className={styles.link}
+          activeClassName={styles.activeLink}
+        >
+          Materials
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={routes.contactsPage}
+          exact
+          className={styles.link}
+          activeClassName={styles.activeLink}
+        >
+          Contacts
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navigation;
