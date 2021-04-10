a0c8d0aa208ef1128d92a69fe67fe40f2c574b91
MyronovychOleksandr <muronalex@ukr.net>
2021-04-10 14:09:16 +0300

    privat authPage

--- src/components/contactsCard/ContactsCard.js
+++ src/components/contactsCard/ContactsCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const ContactsCard = () => {
   return <h3>ContactsCard</h3>;
--- src/components/pages/authPage/AuthPage.js
+++ src/components/pages/authPage/AuthPage.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import AuthForm from "../../authForm/AuthForm";
 import s from "./AuthPage.module.css";
 
-const AuthPage = () => {
-  return (
-    <div className={s.container}>
-      <div className={s.inner}>
-        <div className={s.box}>
-          <h2 className={s.header}>Pro Test</h2>
-          <p className={s.content}>
-            [ We will help you find weak points in knowledge so that you can
-            strengthen it. We will show you what is relevant to know for a{" "}
-            <span className={s.contentAccent}>QA Engineer</span> and will try to
-            make the learning process more diverse_ ]
-          </p>
-        </div>
-        <AuthForm />
-      </div>
-    </div>
-  );
-};
+class AuthPage extends Component {
+  componentDidMount() {
+    if (this.props.isAuthenticated) {
+      this.props.history.replace("/home");
+    }
+  }
 
-export default AuthPage;
+  componentDidUpdate() {
+    if (this.props.isAuthenticated) {
+      this.props.history.replace("/home");
+    }
+  }
+
+  render() {
+    return (
+      <div className={s.container}>
+        <div className={s.inner}>
+          <div className={s.box}>
+            <h2 className={s.header}>Pro Test</h2>
+            <p className={s.content}>
+              [ We will help you find weak points in knowledge so that you can
+              strengthen it. We will show you what is relevant to know for a{" "}
+              <span className={s.contentAccent}>QA Engineer</span> and will try
+              to make the learning process more diverse_ ]
+            </p>
+          </div>
+          <AuthForm />
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.token,
+});
+
+export default connect(mapStateToProps)(AuthPage);
