3e04c78bf33349eb747153be99571e580b558e3c
Valerii Troian <trojan.valerii@gmail.com>
2021-04-12 15:59:57 +0300

    commit for pull new dev

--- src/components/diagram/Diagram.js
+++ src/components/diagram/Diagram.js
@@ -6,47 +6,51 @@
 
 // export default Diagram;
 
-import React from "react";
+import React, { Component } from "react";
 import { Chart } from "react-google-charts";
 import styles from "./Diagram.module.scss";
 
-// const indexChart = window.innerWidth >= 768 ? 160 : 0;
+class Diagram extends Component {
+  state ={
+    data: [
+      ["Task", "Hours per Day"],
+      [`${this.props.percent.slice(0, -1)}%  Correct `, Number(Math.round(this.props.percent.slice(0, -1)/100*12))], //Динамические данные
+      [`${100-this.props.percent.slice(0, -1)}%  Incorrect `, 12-Number(Math.round(this.props.percent.slice(0, -1)/100*12))], //Динамические данные
+    ],
+  }
 
-const Diagram = () => {
-  return (
-    <div className={styles.container}>
-      <Chart
-        className={styles.chart}
-        chartType="PieChart"
-        loader={<div>Loading Chart</div>}
-        data={[
-          ["Task", "Hours per Day"],
-          ["92%  Correct ", 11], //Динамические данные
-          ["8%  Incorrect ", 2], //Динамические данные
-        ]}
-        options={{
-          pieStartAngle: 100,
-          pieSliceText: "none",
-          slices: {
-            0: { color: "#FF6B01" },
-            1: { color: "#D7D7D7" },
-          },
-          chartArea: { left: 100, top: 10, width: "90%", height: "90%" },
-        }}
-        rootProps={{ "data-testid": "1" }}
-      />
-      <div className={styles.answers}>
-        <p className={styles.textAnswers}>
-          Correct answers - <span className={styles.textAnswersNumber}>9</span>
-          {/* Динамические данные */}
-        </p>
-        <p className={styles.textAnswers}>
-          Total questions - <span className={styles.textAnswersNumber}>13</span>
-          {/* Динамические данные */}
-        </p>
+  render() {
+    return (
+      <div className={styles.container}>
+        <Chart
+          className={styles.chart}
+          chartType="PieChart"
+          loader={<div>Loading Chart</div>}
+          data={this.state.data}
+          options={{
+            pieStartAngle: 100,
+            pieSliceText: "none",
+            slices: {
+              0: { color: "#FF6B01" },
+              1: { color: "#D7D7D7" },
+            },
+            chartArea: { left: 100, top: 10, width: "90%", height: "90%" },
+          }}
+          rootProps={{ "data-testid": "1" }}
+        />
+        <div className={styles.answers}>
+          <p className={styles.textAnswers}>
+            Correct answers -{" "}
+            <span className={styles.textAnswersNumber}>{Number(Math.round(this.props.percent.slice(0, -1)/100*12))}</span>
+          </p>
+          <p className={styles.textAnswers}>
+            Total questions -{" "}
+            <span className={styles.textAnswersNumber}>12</span>
+          </p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Diagram;
--- src/redux/reducers/resultsReducer.js
+++ src/redux/reducers/resultsReducer.js
@@ -1,14 +1,35 @@
-import { combineReducers, createReducer } from '@reduxjs/toolkit';
+import { combineReducers, createReducer } from "@reduxjs/toolkit";
+import {
+  resultsRequest,
+  resultsSuccess,
+  resultsError,
+} from "../actions/resultsAction";
 
-const initialUserState = {};
+const initialUserState = {
+  answers: null,
+};
 
-const results = createReducer({ ...initialUserState }, {});
+const results = createReducer(
+  { ...initialUserState },
+  {
+    [resultsSuccess]: (_, { payload }) => payload,
+  }
+);
 
-const error = createReducer(null, {});
+const error = createReducer(null, {
+  [resultsError]: (_, { payload }) => payload,
+});
+
+const loading = createReducer(false, {
+  [resultsRequest]: () => true,
+  [resultsSuccess]: () => false,
+  [resultsError]: () => false,
+});
 
 const resultsReducer = combineReducers({
   results,
   error,
+  loading,
 });
 
 export { resultsReducer };
