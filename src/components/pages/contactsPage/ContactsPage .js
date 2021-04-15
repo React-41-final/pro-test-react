85fb279914d248d61def6c3dd2ac06761fd44143
Alex_Kozzlov <alexandr.kozlov1120@gmail.com>
2021-04-15 20:53:37 +0300

    rebase files off Contactact card

--- src/components/pages/contactsPage/ContactsPage .js
+++ src/components/pages/contactsPage/ContactsPage .js
@@ -1,11 +1,33 @@
-import React from 'react';
-import ContactsCard from '../../contactsCard/ContactsCard';
+import React, { useState } from "react";
+import ContactsCard from "../../contactsCard/ContactsCard";
+
+import s from "./ContactsPage.module.scss";
+import { contacts } from "./data";
+
+// const initialState = [
+//   {
+//     name: "Name",
+//     position: "Front-End Developer",
+//     tasks: "App, Registration, Login, Results page",
+//     email: "aaaa@gmail.com",
+//     linkedin: "https://www.linkedin.com/in/alexkozzlov/",
+//   },
+// ];
 
 const ContactsPage = () => {
+  // const [contacts, setContacts] = useState(initialState);
   return (
-    <div>
-      <h2>Contacts</h2>
-      <ContactsCard />
+    <div className={s.wrapper}>
+      <div className={s.headContainer}>
+        <h2 className={s.head}>Our team</h2>
+      </div>
+      <div className={s.ourTeam}>
+        {contacts.map((item) => (
+          <li>
+            <ContactsCard {...item} />
+          </li>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/pages/contactsPage/data.js
@@ -0,0 +1,58 @@
+import IvanIlch from "../../../img/ourTeam/1.png";
+import AlexKozzlov from "../../../img/ourTeam/2.png";
+import MyronovychOleksandr from "../../../img/ourTeam/3.png";
+import AlexSyzov from "../../../img/ourTeam/4.png";
+import Maryan from "../../../img/ourTeam/5.png";
+import Valerockk from "../../../img/ourTeam/6.png";
+const contacts = [
+  {
+    name: "Ivan Ilchenko",
+    photo: IvanIlch,
+    position: "Front-End Developer",
+    tasks: "App, Registration, Login, Results page",
+    email: "aaaa@gmail.com",
+    linkedin: "https://www.linkedin.com/in/alexkozzlov/",
+  },
+  {
+    name: "Alex Kozlov",
+    photo: AlexKozzlov,
+    position: "Front-End Developer",
+    tasks: "App, Registration, Login, Results page",
+    email: "aaaa@gmail.com",
+    linkedin: "https://www.linkedin.com/in/alexkozzlov/",
+  },
+  {
+    name: "Myronovych Oleksandr",
+    photo: MyronovychOleksandr,
+    position: "Front-End Developer",
+    tasks: "App, Registration, Login, Results page",
+    email: "aaaa@gmail.com",
+    linkedin: "https://www.linkedin.com/in/alexkozzlov/",
+  },
+  {
+    name: "Alex Syzov",
+    photo: AlexSyzov,
+    position: "Front-End Developer",
+    tasks: "App, Registration, Login, Results page",
+    email: "aaaa@gmail.com",
+    linkedin: "https://www.linkedin.com/in/alexkozzlov/",
+  },
+  {
+    name: "Maryan Lashchukievich",
+    photo: Maryan,
+    position: "Front-End Developer",
+    tasks: "App, Registration, Login, Results page",
+    email: "aaaa@gmail.com",
+    linkedin: "https://www.linkedin.com/in/alexkozzlov/",
+  },
+  {
+    name: "Valerii Troian",
+    photo: Valerockk,
+    position: "Front-End Developer",
+    tasks: "App, Registration, Login, Results page",
+    email: "aaaa@gmail.com",
+    linkedin: "https://www.linkedin.com/in/alexkozzlov/",
+  },
+];
+
+export { contacts };
