ad293af0ca23ee498670243f039d9e666fcc708d
MyronovychOleksandr <muronalex@ukr.net>
2021-04-15 13:19:48 +0300

    added social links

--- src/components/contactsCard/ContactsCard.js
+++ src/components/contactsCard/ContactsCard.js
@@ -1,21 +1,49 @@
-import React from "react";
-import s from "./ContactsCard.module.css";
+import React, { Component } from "react";
+import s from "./ContactsCard.module.scss";
 import img from "../../img/chak.jpg";
 
-const ContactsCard = () => {
-  return (
-    <div className={s.container}>
-      <img src={img} className={s.image} alt="profile" />
-      <div className={s.box}>
-        <h3 className={s.name}>NAME</h3>
-        <p className={s.position}>Front-End Developer</p>
-        <p className={s.description}>
-          Lorem Ipsum has been the standard "fish" for Latin texts since the
-          early 16th century.
-        </p>
-      </div>
-    </div>
-  );
-};
+export default class ContactsCard extends Component {
+  state = {
+    isModal: false,
+  };
+  render() {
+    return (
+      <div className={s.container}>
+        <img src={img} className={s.image} alt="profile" />
+        <div className={s.box}>
+          <h3 className={s.name}>NAME</h3>
+          <p className={s.position}>Front-End Developer</p>
+          <p className={s.description}>
+            Lorem Ipsum has been the standard "fish" for Latin texts since the
+            early 16th century.
+          </p>
+          <div className={s.socialContainer}>
+            <a
+              href="linkedin"
+              className={`${s.link} ${s.linkedin}`}
+              target="_blanck"
+            >
+              <span className={s.visuallyHidden}>linkedin</span>
+            </a>
 
-export default ContactsCard;
+            <a
+              href="facebook"
+              className={`${s.link} ${s.facebook}`}
+              target="_blanck"
+            >
+              <span className={s.visuallyHidden}>Facebook</span>
+            </a>
+
+            <a
+              href="mailto:email"
+              className={`${s.link} ${s.email}`}
+              target="_blanck"
+            >
+              <span className={s.visuallyHidden}>Email</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
